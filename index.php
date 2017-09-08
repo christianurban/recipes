383cacb6ff941dc44b3c530915b4e3dca4375eb6
christianurban <urban.christian@web.de>
2017-09-08 14:48:24 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+  </head>
+  <body>
+	<?php 
+		#header('Content-Type: text/html; charset=ISO-8859-1');
+		include "navbar.html"; 
+	?>
+	<?php 
+		include "body.html"; 
+	?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ mysql.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "christianurban";
+$password = "Cu190586!";
+$dbname = "christianurban";
+?>
